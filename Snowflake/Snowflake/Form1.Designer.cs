a741b5a1bfb4d3a55e716dab9595ef287b9d6a9c
Steve Ratering <s.ratering@sbcglobal.net>
2020-05-25 18:53:42 -0500

    the whole folder, how nice

---
+++ Snowflake/Snowflake/Form1.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace Snowflake
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.myShapeO = new System.Windows.Forms.RadioButton();
+            this.myShapeSnow = new System.Windows.Forms.RadioButton();
+            this.myLevel = new System.Windows.Forms.TextBox();
+            this.myColor = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.canvas = new System.Windows.Forms.Panel();
+            this.myNested = new System.Windows.Forms.CheckBox();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.myNested);
+            this.panel1.Controls.Add(this.myShapeO);
+            this.panel1.Controls.Add(this.myShapeSnow);
+            this.panel1.Controls.Add(this.myLevel);
+            this.panel1.Controls.Add(this.myColor);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 63);
+            this.panel1.TabIndex = 0;
+            // 
+            // myShapeO
+            // 
+            this.myShapeO.AutoSize = true;
+            this.myShapeO.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.myShapeO.Location = new System.Drawing.Point(135, 11);
+            this.myShapeO.Name = "myShapeO";
+            this.myShapeO.Size = new System.Drawing.Size(81, 20);
+            this.myShapeO.TabIndex = 6;
+            this.myShapeO.Text = "O Curve";
+            this.myShapeO.UseVisualStyleBackColor = true;
+            this.myShapeO.CheckedChanged += new System.EventHandler(this.myShapeO_CheckedChanged);
+            // 
+            // myShapeSnow
+            // 
+            this.myShapeSnow.AutoSize = true;
+            this.myShapeSnow.Checked = true;
+            this.myShapeSnow.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.myShapeSnow.Location = new System.Drawing.Point(32, 11);
+            this.myShapeSnow.Name = "myShapeSnow";
+            this.myShapeSnow.Size = new System.Drawing.Size(97, 20);
+            this.myShapeSnow.TabIndex = 5;
+            this.myShapeSnow.TabStop = true;
+            this.myShapeSnow.Text = "Snowflake";
+            this.myShapeSnow.UseVisualStyleBackColor = true;
+            this.myShapeSnow.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // myLevel
+            // 
+            this.myLevel.Location = new System.Drawing.Point(452, 27);
+            this.myLevel.Name = "myLevel";
+            this.myLevel.Size = new System.Drawing.Size(51, 20);
+            this.myLevel.TabIndex = 3;
+            this.myLevel.Text = "0";
+            this.myLevel.TextChanged += new System.EventHandler(this.myLevel_TextChanged);
+            this.myLevel.KeyDown += new System.Windows.Forms.KeyEventHandler(this.myLevel_KeyDown);
+            this.myLevel.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.myLevel_KeyPress);
+            // 
+            // myColor
+            // 
+            this.myColor.Location = new System.Drawing.Point(317, 27);
+            this.myColor.Name = "myColor";
+            this.myColor.Size = new System.Drawing.Size(51, 20);
+            this.myColor.TabIndex = 2;
+            this.myColor.Text = "Blue";
+            this.myColor.TextChanged += new System.EventHandler(this.color_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(392, 31);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "level";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(256, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "color";
+            // 
+            // canvas
+            // 
+            this.canvas.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.canvas.Location = new System.Drawing.Point(0, 63);
+            this.canvas.Name = "canvas";
+            this.canvas.Size = new System.Drawing.Size(800, 387);
+            this.canvas.TabIndex = 1;
+            this.canvas.Paint += new System.Windows.Forms.PaintEventHandler(this.panel2_Paint);
+            // 
+            // myNested
+            // 
+            this.myNested.AutoSize = true;
+            this.myNested.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.myNested.Location = new System.Drawing.Point(32, 40);
+            this.myNested.Name = "myNested";
+            this.myNested.Size = new System.Drawing.Size(74, 20);
+            this.myNested.TabIndex = 7;
+            this.myNested.Text = "nested";
+            this.myNested.UseVisualStyleBackColor = true;
+            this.myNested.CheckedChanged += new System.EventHandler(this.myNested_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.canvas);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel canvas;
+        private System.Windows.Forms.TextBox myLevel;
+        private System.Windows.Forms.TextBox myColor;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RadioButton myShapeSnow;
+        private System.Windows.Forms.RadioButton myShapeO;
+        private System.Windows.Forms.CheckBox myNested;
+    }
+}
+
---
+++ Snowflake/Snowflake/Form1.cs
@@ -0,0 +1,249 @@
+﻿// Steve Ratering
+// 2020/5/25
+// Draw Snowflake Curve and O Curve
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Snowflake
+{
+    public partial class Form1 : Form
+    {
+        Pen myPen = new Pen(Color.FromName("Blue"));
+        Graphics g;
+        static int width, height, level;
+        static double length;
+        static String shape;
+        static Boolean nested;
+
+        public Form1()
+        {
+            InitializeComponent();
+            shape = "Snow";
+        }
+
+        private void DrawSnow(double x, double y, int direction, double length, int level, Boolean incr)
+        {
+            double x1, y1;
+            if (level == 0) {
+                x1 = x + length * Math.Cos(direction * Math.PI / 6);
+                y1 = y - length * Math.Sin(direction * Math.PI / 6);
+                g.DrawLine(myPen, (int)x, (int)y, (int)(x1), (int)(y1));
+            }
+            else
+            {
+                double newLen = length / Math.Sqrt(3);
+                if (incr)
+                {
+                    DrawSnow(x, y, (direction + 1) % 12, newLen, level - 1, false);
+                    x1 = x + newLen * Math.Cos((direction + 1) * Math.PI / 6);
+                    y1 = y - newLen * Math.Sin((direction + 1) * Math.PI / 6);
+                    DrawSnow(x1, y1, (direction + 11) % 12, newLen, level - 1, false);
+                }
+                else
+                {
+                    DrawSnow(x, y, (direction + 11) % 12, newLen, level - 1, true);
+                    x1 = x + newLen * Math.Cos((direction + 11) * Math.PI / 6);
+                    y1 = y - newLen* Math.Sin((direction + 11) * Math.PI / 6);
+                    DrawSnow(x1, y1, (direction + 1) % 12, newLen, level - 1, true);
+                }
+            }
+        }
+
+        private void DrawO(double x, double y, int direction, double length, int level)
+        {
+            double x1, y1;
+
+            if (level == 0)
+            {
+                x1 = x + length * Math.Cos(direction * Math.PI / 4);
+                y1 = y - length * Math.Sin(direction * Math.PI / 4);
+                g.DrawLine(myPen, (int)Math.Round(x), (int)Math.Round(y), (int)Math.Round(x1), (int)Math.Round(y1));
+            }
+            else
+            {
+                double newLen = length / Math.Sqrt(2);
+                DrawO(x, y, (direction + 1) % 8, newLen, level - 1);
+                x1 = x + newLen * Math.Cos((direction + 1) % 8 * Math.PI / 4);
+                y1 = y - newLen * Math.Sin((direction + 1) % 8 * Math.PI / 4);
+                DrawO(x1, y1, (direction + 7) % 8, newLen, level - 1);
+            }
+        }
+
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+            //float thick = float.Parse(myThickness.Text);
+            float thick = 1;
+            width = canvas.Width;
+            height = canvas.Height;
+            if (shape == "Snow")
+            {
+                length = Math.Min((width - 2 * thick) / 3, (height - 2 * thick) / 2 / Math.Sqrt(3));
+            }
+            else
+            {
+                length = Math.Min(width / 2, height / 2);
+            }
+            myPen = new Pen(Color.FromName(myColor.Text), thick);
+            //myPen.StartCap = System.Drawing.Drawing2D.LineCap.Round;
+            //myPen.EndCap = System.Drawing.Drawing2D.LineCap.Round;
+            g = canvas.CreateGraphics();
+            double centerX, centerY, x1, y1, x2, y2, x3, y3;
+            centerX = canvas.Width / 2;
+            centerY = canvas.Height / 2;
+            g.Clear(Color.White);
+            if (shape == "Snow")
+            {
+                x1 = centerX - length * 1.5;
+                y1 = centerY + length * Math.Sqrt(3) / 2;
+                x2 = centerX;
+                y2 = centerY - length * Math.Sqrt(3);
+                x3 = centerX + length * 1.5;
+                DrawSnow(x1, y1, 2, length * 3, level, true);
+                DrawSnow(x2, y2, 10, length * 3, level, true);
+                DrawSnow(x3, y1, 6, length * 3, level, true);
+                if (nested && level > 1)
+                {
+                    x1 = centerX - length;
+                    y1 = centerY;
+                    x2 = centerX + length / 2;
+                    y2 = centerY - length * Math.Sqrt(3) / 2;
+                    y3 = centerY + length * Math.Sqrt(3) / 2;
+                    DrawSnow(x1, y1, 1, length * Math.Sqrt(3), level - 1, true);
+                    DrawSnow(x2, y2, 9, length * Math.Sqrt(3), level - 1, true);
+                    DrawSnow(x2, y3, 5, length * Math.Sqrt(3), level - 1, true);
+                }
+            }
+            else
+            {
+                x1 = centerX - length / 2;
+                y1 = centerY;
+                x2 = centerX + length / 2;
+                DrawO(x1, y1, 0, length, level);
+                DrawO(x2, y1, 4, length, level);
+            }
+        }
+
+        private void myLevel_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                level = Int32.Parse(myLevel.Text);
+                if (level > 12)
+                {
+                    level = 12;
+                    myLevel.Text = "12";
+                }
+                if (level < 0)
+                {
+                    level = 0;
+                    myLevel.Text = "0";
+                }
+                canvas.Refresh();
+            }
+            catch
+            {
+                myLevel.Text = "";
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+        }
+        private void color_TextChanged(object sender, EventArgs e)
+        {
+            Color c = Color.FromName(myColor.Text);
+            if (c.A > 0 || c.R > 0 || c.G > 0 || c.B > 0)
+            {
+                canvas.Refresh();
+            }
+        }
+
+        private void myLevel_KeyPress(object sender, KeyPressEventArgs e)
+        {
+        }
+
+        private void myLevel_KeyDown(object sender, KeyEventArgs e)
+        {
+            const int up = 38;
+            const int down = 40;
+            int localLevel;
+            try
+            {
+                localLevel = Int32.Parse(myLevel.Text);
+                if (e.KeyValue == up && localLevel < 12)
+                {
+                    localLevel++;
+                    myLevel.Text = "" + localLevel;
+                }
+                if (e.KeyValue == down && localLevel > 0)
+                {
+                    localLevel--;
+                    myLevel.Text = "" + localLevel;
+                }
+            }
+            catch
+            {
+            }
+        }
+
+        private void myShape_KeyDown(object sender, KeyEventArgs e)
+        {
+        }
+
+        private void myShape_KeyUp(object sender, KeyEventArgs e)
+        {
+        }
+
+        private void myShape_KeyPress(object sender, KeyPressEventArgs e)
+        {
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        /* the link between Form1[Design] and Form1.Designer didn't change this method name to myShapeSnow_CheckedChanged */
+        {
+            if (myShapeSnow.Checked)
+            {
+                shape = "Snow";
+                canvas.Refresh();
+            }
+        }
+
+        private void myShapeO_CheckedChanged(object sender, EventArgs e)
+        {
+            if (myShapeO.Checked) 
+            {
+                shape = "O";
+                canvas.Refresh();
+            }
+        }
+
+        private void myNested_CheckedChanged(object sender, EventArgs e)
+        {
+            nested = myNested.Checked;
+            canvas.Refresh();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            canvas.Refresh();
+        }
+    }
+}
